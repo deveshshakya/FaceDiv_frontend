dc4a9dc70905c2faecb091f6bda58b0784f9f363
Devesh Shakya <deveshshakya773@gmail.com>
2020-08-12 20:13:41 +0530

    Updates

---
+++ precache-manifest.78cba152dbdb2ebe1da3dbc261b87228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa8a61177c30edc26ed8d006d9274c9",
+    "url": "/facediv/index.html"
+  },
+  {
+    "revision": "2eb019fd1e6f3f760f7c",
+    "url": "/facediv/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d3e8f0e77aa7e12c79e7",
+    "url": "/facediv/static/css/main.e24d29f3.chunk.css"
+  },
+  {
+    "revision": "2eb019fd1e6f3f760f7c",
+    "url": "/facediv/static/js/2.14b7642b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facediv/static/js/2.14b7642b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e8f0e77aa7e12c79e7",
+    "url": "/facediv/static/js/main.635ac3bd.chunk.js"
+  },
+  {
+    "revision": "fd6925dacf2ee5d4be5a",
+    "url": "/facediv/static/js/runtime-main.d643e73c.js"
+  }
+]);
